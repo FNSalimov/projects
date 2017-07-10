163f3782e716a8a8da998b0bbacee8d808af3a93
Your Name <you@example.com>
2017-07-10 04:37:31 +0300

    done

--- english/static/js/ping.js
+++ english/static/js/ping.js
@@ -14,5 +14,17 @@ jQuery('document').ready(function() {
 		jQuery('#ball').offset({top:(top_flag)?val.top+10:val.top-10, left:(left_flag)?val.left+10:val.left-10});
 	}
 	setInterval(ball, 10);
-
+	jQuery(document).keyup(function(e) {
+		var valleft = jQuery('#left').offset();
+		var valright = jQuery('#right').offset();
+		var win_height = jQuery(window).height();
+		if (e.which == 87 && valleft.top > 0)
+			jQuery('#left').offset({top: valleft.top-20, left: valleft.left});
+		if (e.which == 83 && valleft.top < win_height - 160)
+			jQuery('#left').offset({top: valleft.top+20, left: valleft.left});
+		if (e.which == 38 && valright.top > 0)
+			jQuery('#right').offset({top: valright.top-20, left: valright.left});
+		if (e.which == 40 && valright.top < win_height - 160)
+			jQuery('#right').offset({top: valright.top+20, left: valright.left});
+	});
 });
