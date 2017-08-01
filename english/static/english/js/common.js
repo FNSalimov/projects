f440d8133d87cd4802fb0a45fb3412743af29c8b
Your Name <you@example.com>
2017-08-01 15:48:43 +0300

    done

---
+++ english/static/english/js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+	var flag = 1;
+	$(".blue").click(function() {
+		if (flag) {
+			$(".icons").fadeIn(1000);
+			flag = 0;
+		} else {
+			$(".icons").fadeOut(1000);
+			flag = 1;
+		}
+		console.log(flag);
+	})
+});
